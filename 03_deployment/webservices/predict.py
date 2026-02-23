ead960387e81e04e83a34510cb5184a1c2fa552d
avafor <forooz.shahbazi@gmail.com>
2026-02-23 17:19:37 +0100

    cleaned up predict

--- 03_deployment/webservices/predict.py
+++ 03_deployment/webservices/predict.py
@@ -8,32 +8,16 @@ from typing import Optional
 from pathlib import Path
 
 
-# Point MLflow explicitly to the same store your notebook uses
-mlruns_path = Path("/Users/iashahba/Documents/ARTIFIN/FS26/Data and Code/01_model_tracking/mlruns")
-uri = mlruns_path.as_uri()  # handles spaces safely
+mlflow.set_tracking_uri(os.environ["MLFLOW_TRACKING_URI"])
 
-mlflow.set_tracking_uri(uri)
-mlflow.set_registry_uri(uri)
-
-print("CWD:", os.getcwd())
-print("MLflow tracking:", mlflow.get_tracking_uri())
-print("MLflow registry:", mlflow.get_registry_uri())
-from pathlib import Path
-import mlflow
+scaler = joblib.load('/Users/iashahba/Documents/ARTIFIN/FS26/Data and Code/01_model_tracking/scaler.joblib')
 
 MODEL_DIR = Path(
     "/Users/iashahba/Documents/ARTIFIN/FS26/Data and Code/01_model_tracking/mlruns/"
     "316585724729739867/models/m-a44d65f42a0f4e54a4d05dc9c30409cd/artifacts"
 )
-
+loaded_model = mlflow.pyfunc.load_model(MODEL_DIR.as_posix())
 def predict(input_data):
-    
-    # load the scaler
-    scaler = joblib.load('/Users/iashahba/Documents/ARTIFIN/FS26/Data and Code/01_model_tracking/scaler.joblib')
-
-    
-    model_uri = "models:/iris_baseline/Staging"
-    loaded_model = mlflow.pyfunc.load_model(MODEL_DIR.as_posix())
 
     # apply the scaler to the input data
     input_data = scaler.transform(input_data)
@@ -49,7 +33,6 @@ class PredictRequest(BaseModel):
 @app.post("/predict")
 def predict_endpoint(req: PredictRequest):
     input_array = np.array(req.input_data).reshape(1, -1)
-    #RUN_ID = req.run_id
     prediction = predict(input_array)
     return {"prediction": prediction.tolist()}
 
