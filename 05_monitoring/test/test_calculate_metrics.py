1269442842600945d2bff9a8dfab0c60e4001036
avafor <forooz.shahbazi@gmail.com>
2026-05-05 11:22:59 +0200

    added test to monitoring

---
+++ 05_monitoring/test/test_calculate_metrics.py
@@ -0,0 +1,22 @@
+def test_features_defined():
+    import scripts.calculate_metrics as cm
+    assert hasattr(cm, "FEATURES"), "FEATURES is not defined"
+
+
+def test_features_structure():
+    from scripts.calculate_metrics import FEATURES
+    assert isinstance(FEATURES, list)
+    assert len(FEATURES) > 0
+
+
+def test_features_are_correct():
+    from scripts.calculate_metrics import FEATURES
+
+    expected = [
+        "sepal_length",
+        "sepal_width",
+        "petal_length",
+        "petal_width"
+    ]
+
+    assert FEATURES == expected
